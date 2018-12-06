a1015de7fe3e44b1ff1eff927af08586f0f7ea84
Andie <zchangan@163.com>
2018-12-06 13:48:17 +0800

    :truck: Rename test files

---
+++ test/redundancy.test.ts
@@ -0,0 +1,57 @@
+import assert = require('assert');
+import {KevastAsync, KevastSync} from '../src/index';
+import {AStorage} from './util/AStorage';
+import {SStorage} from './util/SStorage';
+type Storage = AStorage | SStorage;
+
+describe('Test redundancy', () => {
+  it('Main sync redundancy sync', () => {
+    const map1 = new Map<string, string>();
+    const map2 = new Map<string, string>();
+    const kevast = new KevastSync(new SStorage(map1), new SStorage(map2));
+    kevast.set(Math.random().toString(), Math.random().toString());
+    assert.deepEqual(map1, map2);
+  });
+  it('Main sync redundancy async', async () => {
+    const map1 = new Map<string, string>();
+    const map2 = new Map<string, string>();
+    const kevast = new KevastAsync(new SStorage(map1), new AStorage(map2));
+    await kevast.set(Math.random().toString(), Math.random().toString());
+    assert.deepEqual(map1, map2);
+  });
+  it('Main async redundancy sync', async () => {
+    const map1 = new Map<string, string>();
+    const map2 = new Map<string, string>();
+    const kevast = new KevastAsync(new AStorage(map1), new SStorage(map2));
+    await kevast.set(Math.random().toString(), Math.random().toString());
+    assert.deepEqual(map1, map2);
+  });
+  it('Main async redundancy async', async () => {
+    const map1 = new Map<string, string>();
+    const map2 = new Map<string, string>();
+    const kevast = new KevastAsync(new AStorage(map1), new AStorage(map2));
+    kevast.set(Math.random().toString(), Math.random().toString());
+    assert.deepEqual(map1, map2);
+  });
+  it('Random storage', async () => {
+    const maps: Array<Map<string, string>> = [];
+    const storages: Storage[] = [];
+    for (let i = 0; i < 10; i++) {
+      const map = new Map();
+      maps.push(map);
+      storages.push(Math.random() > 0.5 ? new SStorage(map) : new AStorage(map));
+    }
+    if (storages.every((storage) => storage instanceof SStorage)) {
+      const map = new Map();
+      maps.push(map);
+      storages.push(new AStorage(map));
+    }
+    const kevast = new KevastAsync(storages[0], ...storages.slice(1));
+    for (let i = 0; i < 10; i++) {
+      kevast.set(Math.random().toString(), Math.random().toString());
+    }
+    for (let i = 1; i < maps.length; i++) {
+      assert.deepEqual(maps[i - 1], maps[i]);
+    }
+  });
+});
